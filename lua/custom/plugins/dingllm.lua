7b4fb1869bad5c5e8d42c1829236e31fa65c0849
0xtito <welovethepenguins@gmail.com>
2024-08-02 11:41:46 -0400

    added dingllm locally

---
+++ lua/custom/plugins/dingllm.lua
@@ -0,0 +1,10 @@
+return {
+  dir = config_path(vim.fn.stdpath 'config', 'lua', 'custom', 'llm.lua'),
+  name = 'dingllm-local',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('custom.dingllm.config').setup()
+  end,
+}
